6a312fb05caa2033fe06dcf649af9569a5458cd8
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-04-03 18:12:56 -0500

    Feature: Added settings.lua
    This file contains most of my global settings. Autocommands.lua will
    contain the per-filetype settings. This file is referenced in init.vim.

---
+++ lua/settings.lua
@@ -0,0 +1,25 @@
+-- Style
+vim.opt.background="dark"
+vim.opt.termguicolors=true
+vim.opt.title=true
+
+-- Editing
+vim.opt.expandtab=true
+vim.opt.joinspaces=false -- Don't adjoin two sentences with two space characters as is default.
+vim.opt.shiftwidth=2
+vim.opt.smarttab=true
+vim.opt.softtabstop=0
+vim.opt.tabstop=2
+
+-- Usability
+vim.opt.hlsearch=true
+vim.opt.incsearch=true
+vim.opt.list=true
+vim.opt.listchars="tab:<=>,trail:-"
+vim.opt.mouse="a"
+vim.opt.number=true
+vim.opt.wrap=false
+
+-- Misc
+vim.opt.dictionary=vim.opt.dictionary+"/usr/share/dict/words" -- In insert mode, C-X, C-K will bring up the completion list if omnifunc is enabled
+vim.opt.tags="./tags,tags,/home/sam/.commontags/tags"
