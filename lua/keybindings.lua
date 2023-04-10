99e045fe0a55d72233eac1eb891d3808e91ae166
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-04-10 16:05:41 -0500

    Documentation: Added description strings to mappings
    These strings will allow me to programmatically determine the purpose of
    a key binding.

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,24 +1,26 @@
 -- Use :map to list key bindings
 
 -- Save and quit
-vim.keymap.set('n', 's', ':w<cr>')
-vim.keymap.set('n', 'Q', ':q<cr>')
+vim.keymap.set('n', 's', ':w<cr>', {desc = "Save using s"})
+vim.keymap.set('n', 'Q', ':q<cr>', {desc = "Quit using Q"})
 
--- Run VimScript on current line
-vim.keymap.set('n', '<leader>D', 'yy@"')
+vim.keymap.set('n', '<leader>D', 'yy@"', {desc = "Run VimScript on current line"})
 
--- Copy syntax-highlighted text to HTML
 vim.g.html_number_lines = 0
-vim.keymap.set('v', '<leader>C', ':TOhtml<cr>/vimCodeElement<cr>V/<\\/pre><cr>"*y:q!<cr>')
+vim.keymap.set('v', '<leader>C', ':TOhtml<cr>/vimCodeElement<cr>V/<\\/pre><cr>"*y:q!<cr>', {desc = "Copy syntax-highlighted text to HTML"})
 
--- Format using clang-format
-vim.keymap.set('n', '<leader>F', "mm:%!clang-format<cr>'mzz")
+vim.keymap.set('n', '<leader>F', "mm:%!clang-format<cr>'mzz", {desc = "Format using clang-format"})
 
 -- Telescope
-vim.keymap.set('n', '<leader>fG', ':Telescope grep_string search=<c-r><c-w><cr>')
-vim.keymap.set('n', '<leader>fb', ':Telescope buffers<cr>')
-vim.keymap.set('n', '<leader>fb', ':Telescope builtin<cr>')
-vim.keymap.set('n', '<leader>fc', ':Telescope colorscheme<cr>')
-vim.keymap.set('n', '<leader>ff', ':Telescope find_files<cr>')
-vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<cr>')
-vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<cr>')
+vim.keymap.set('n', '<leader>fG', ':Telescope grep_string search=<c-r><c-w><cr>', {desc = "Search for word under cursor"})
+vim.keymap.set('n', '<leader>fb', ':Telescope buffers<cr>', {desc = "Search for buffers"})
+vim.keymap.set('n', '<leader>fb', ':Telescope builtin<cr>', {desc = "Search for builtin commands"})
+vim.keymap.set('n', '<leader>fc', ':Telescope colorscheme<cr>', {desc = "Search for colorschemes"})
+vim.keymap.set('n', '<leader>ff', ':Telescope find_files<cr>', {desc = "Search for files"})
+vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<cr>', {desc = "Search for text"})
+vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<cr>', {desc = "Search for help tags"})
+
+-- " Ctrl-\, Ctrl-n to get out of insert mode
+vim.keymap.set('n', '<leader>u', '<c-w>n<c-w>L:term sc-im<cr>:set nonu<cr>', {desc = "Open a terminal with sc-im in a new window"})
+
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', {desc = "Exit terminal insert mode using escape"})
