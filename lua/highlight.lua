2a6e0b837a2d7669766562d10f1c78786d5744c7
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-04-20 16:18:37 -0500

    Formatting: General cleanup
    I've changed some of the spacing and formatting, and checked in all
    outstanding changes. This will make the code a bit more legible.

--- lua/highlight.lua
+++ lua/highlight.lua
@@ -8,9 +8,9 @@ vim.cmd("hi Search cterm=underline ctermfg=0 ctermbg=11 gui=underline guifg=#f0a
 
 -- Background
 vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE")
 
 -- Status bar
-vim.cmd("hi BufferlineSelected guifg=#440000 guibg=#888888 ctermfg=0 ctermbg=46 gui=underline")
 vim.cmd("hi StatusAccent guifg=#222222 guibg=#888888 ctermfg=0 ctermbg=46")
 vim.cmd("hi StatusMain guifg=#888888 guibg=#222222 ctermbg=46 ctermfg=0")
 
--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,34 +1,35 @@
 -- Use :map to list key bindings
 
--- Save and quit
-vim.keymap.set('n', 's', ':w<cr>', {desc = "Save using s"})
-vim.keymap.set('n', 'Q', ':q<cr>', {desc = "Quit using Q"})
-
-vim.keymap.set('n', '<leader>D', 'yy@"', {desc = "Run VimScript on current line"})
-
-vim.g.html_number_lines = 0
-vim.keymap.set('v', '<leader>C', ':TOhtml<cr>/vimCodeElement<cr>V/<\\/pre><cr>"*y:q!<cr>', {desc = "Copy syntax-highlighted text to HTML"})
-
 -- Telescope
 vim.keymap.set('n', '<leader>fG', ':Telescope grep_string search=<c-r><c-w><cr>', {desc = "Search for word under cursor"})
-vim.keymap.set('n', '<leader>fb', ':Telescope buffers<cr>', {desc = "Search for buffers"})
-vim.keymap.set('n', '<leader>fb', ':Telescope builtin<cr>', {desc = "Search for builtin commands"})
-vim.keymap.set('n', '<leader>fc', ':Telescope colorscheme<cr>', {desc = "Search for colorschemes"})
-vim.keymap.set('n', '<leader>ff', ':Telescope find_files<cr>', {desc = "Search for files"})
-vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<cr>', {desc = "Search for text"})
-vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<cr>', {desc = "Search for help tags"})
+vim.keymap.set('n', '<leader>fb', ':Telescope buffers<cr>',                       {desc = "Search for buffers"})
+vim.keymap.set('n', '<leader>fb', ':Telescope builtin<cr>',                       {desc = "Search for builtin commands"})
+vim.keymap.set('n', '<leader>ff', ':Telescope find_files<cr>',                    {desc = "Search for files"})
+vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<cr>',                     {desc = "Search for text"})
+vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<cr>',                     {desc = "Search for help tags"})
 
--- " Ctrl-\, Ctrl-n to get out of insert mode
-vim.keymap.set('n', '<leader>u', '<c-w>n<c-w>L:term sc-im<cr>:set nonu<cr>', {desc = "Open a terminal with sc-im in a new window"})
+vim.keymap.set('n', '<leader>fc', function()
+  vim.cmd(':Telescope colorscheme');
+end, {desc = "Search for colorschemes"})
+vim.keymap.set('n', '<leader>fC', function()
+  vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
+  vim.cmd('hi EndOfBuffer guibg=NONE ctermbg=NONE')
+end, {desc = "Reset background to be transparent"})
 
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', {desc = "Exit terminal insert mode using escape"})
-
-vim.keymap.set('n', '<F6>', ':DiffviewFileHistory %<cr>', {desc = "Open file history"})
+-- Miscellanous
+vim.keymap.set('n', '<F6>',      ':DiffviewFileHistory %<cr>',                             {desc = "Open file history"})
+vim.keymap.set('n', '<leader>D', 'yy@"',                                                   {desc = "Run VimScript on current line"})
+vim.keymap.set('n', '<leader>u', '<c-w>n<c-w>L:term sc-im<cr>:set nonu<cr>',               {desc = "Open a terminal with sc-im in a new window"})
+vim.keymap.set('n', 'n',         'nzz',                                                    {desc = "Auto-center when searching for matches"})
+vim.keymap.set('n', 's',         ':w<cr>',                                                 {desc = "Save using s"})
+vim.keymap.set('t', '<Esc>',     '<C-\\><C-n>',                                            {desc = "Exit terminal insert mode using escape"})
+vim.keymap.set('v', '<leader>C', ':TOhtml<cr>/vimCodeElement<cr>V/<\\/pre><cr>"*y:q!<cr>', {desc = "Copy syntax-highlighted text to HTML"})
 
 -- Bindings for C/C++
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
   pattern = {"*.c", "*.cpp"},
   callback = function()
+    vim.keymap.set('n', '<F5>', ':make run<cr>', {desc = "Run make"})
     vim.keymap.set('n', '<leader>F', "mm:%!clang-format<cr>'mzz", {desc = "Format using clang-format"})
   end,
 })
