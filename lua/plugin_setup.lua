d880c426b6275834238cf6eca5cdeab94e3acc59
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-04-22 13:12:17 -0500

    Bug fix: Added missing config for nvim-notify
    I was getting the following message when using nvim-notify:
    
    Highlight group 'NotifyBackground' has no background highlight
    Please provide an RGB hex value or highlight group with a background
    value for 'background_colour' option.
    This is the colour that will be used for 100% transparency.
    ```lua
    require("notify").setup({
      background_colour = "#000000",
    })
    ```
    Defaulting to #000000
    Press ENTER or type command to continue
    
    Adding the configuration to the plugin_setup module fixes this.

--- lua/plugin_setup.lua
+++ lua/plugin_setup.lua
@@ -466,3 +466,7 @@ require('orgmode').setup({
   org_agenda_files = {'~/org/*'},
   org_default_notes_file = '~/org/refile.org',
 })
+
+require("notify").setup({
+  background_colour = "#000000",
+})
