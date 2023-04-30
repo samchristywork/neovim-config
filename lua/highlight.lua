35da5e4430c5801d65c10699576b3b525362351a
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-04-29 20:56:22 -0500

    chore: Make parts of the bar transparent
    In order to fit in stylistically with the rest of my development setup,
    I've changed the background color on my status bar to be transparent.
    This color is maybe poorly named, it is the part of the status bar that
    doesn't have text written on it.

--- lua/highlight.lua
+++ lua/highlight.lua
@@ -12,7 +12,7 @@ vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE")
 
 -- Status bar
 vim.cmd("hi StatusAccent guifg=#222222 guibg=#888888 ctermfg=0 ctermbg=46")
-vim.cmd("hi StatusMain guifg=#888888 guibg=#222222 ctermbg=46 ctermfg=0")
+vim.cmd("hi StatusMain guifg=#888888 guibg=NONE ctermbg=46 ctermfg=0")
 
 -- Selection
 vim.cmd("hi Visual cterm=underline ctermfg=186 ctermbg=64 gui=underline guifg=#5f8700 guibg=#071000")
