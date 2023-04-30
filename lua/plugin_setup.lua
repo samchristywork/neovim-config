7425ed0f4b8d6cc27eb1c6f6defff939d6ad3ee4
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-04-29 20:46:33 -0500

    refactor: Move nvim-cmp section up and remove org-mode
    The nvim-cmp section had to be moved higher in the file. Not entirely
    sure why but I got in a state where completion wouldn't work anymore and
    this fixed it.
    
    Also, get rid of org-mode because it is obsoleted by other tools like
    taskwarrior, taskell, and time tracking software that I have built
    myself.

--- lua/plugin_setup.lua
+++ lua/plugin_setup.lua
@@ -5,6 +5,59 @@ require("scrollbar").setup()
 require("bufferline").setup()
 require("messages").setup()
 
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+    -- C-b (back) C-f (forward) for snippet placeholder navigation.
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<Tab>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    -- ['<Tab>'] = cmp.mapping(function(fallback)
+    --   if cmp.visible() then
+    --     cmp.select_next_item()
+    --   elseif luasnip.expand_or_jumpable() then
+    --     luasnip.expand_or_jump()
+    --   else
+    --     fallback()
+    --   end
+    -- end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+    { name = 'emoji', insert=true },
+    { name = 'calc' },
+    { name = 'conventionalcommits' },
+    { name = 'nerdfont' },
+    -- { name = 'dictionary' },
+    -- { name = 'digraphs' },
+  },
+}
+
 require('vgit').setup({
   keymaps = {
     ['n <C-k>'] = function() require('vgit').hunk_up() end,
@@ -308,59 +361,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
--- luasnip setup
-local luasnip = require 'luasnip'
-
--- nvim-cmp setup
-local cmp = require 'cmp'
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
-    -- C-b (back) C-f (forward) for snippet placeholder navigation.
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<Tab>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    -- ['<Tab>'] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_next_item()
-    --   elseif luasnip.expand_or_jumpable() then
-    --     luasnip.expand_or_jump()
-    --   else
-    --     fallback()
-    --   end
-    -- end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  }),
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'buffer' },
-    { name = 'path' },
-    { name = 'emoji', insert=true },
-    { name = 'calc' },
-    { name = 'conventionalcommits' },
-    { name = 'nerdfont' },
-    -- { name = 'dictionary' },
-    -- { name = 'digraphs' },
-  },
-}
-
 require('dap').adapters.codelldb = {
   type = 'server',
   port = "1234",
@@ -459,12 +459,6 @@ require("nvim-tree").setup({
   filters = {
     dotfiles = true,
   },
-})
-
-require('orgmode').setup_ts_grammar()
-require('orgmode').setup({
-  org_agenda_files = {'~/org/*'},
-  org_default_notes_file = '~/org/refile.org',
 })
 
 require("notify").setup({
