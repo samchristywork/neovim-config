812865143c16c219bd503ba67ffcc60039738bf5
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-05-01 18:51:16 -0500

    feat: Add bufferline configuration
    This is close to the default configuration, but I still want it inserted
    here explicitly so that I can easily view and change the defaults.

--- lua/plugin_setup.lua
+++ lua/plugin_setup.lua
@@ -58,6 +58,40 @@ cmp.setup {
   },
 }
 
+require('bufferline').setup {
+    options = {
+        mode = "buffers", -- set to "tabs" to only show tabpages instead
+        numbers = "none",
+        close_command = "bdelete! %d",       -- can be a string | function, | false see "Mouse actions"
+        right_mouse_command = "bdelete! %d", -- can be a string | function  | false see "Mouse actions"
+        left_mouse_command = "buffer %d",    -- can be a string | function, | false see "Mouse actions"
+        middle_mouse_command = nil,          -- can be a string | function, | false see "Mouse actions"
+        indicator = {
+            icon = '▎', -- this should be omitted if indicator style is not 'icon'
+            style = 'icon',
+        },
+        buffer_close_icon = '',
+        modified_icon = '●',
+        close_icon = '',
+        left_trunc_marker = '',
+        right_trunc_marker = '',
+        max_name_length = 18,
+        max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+        truncate_names = true, -- whether or not tab names should be truncated
+        tab_size = 18,
+        diagnostics = "nvim_lsp",
+        diagnostics_update_in_insert = false,
+        -- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
+        color_icons = true, -- whether or not to add the filetype icon highlights
+        show_buffer_icons = true, -- disable filetype icons for buffers
+        show_buffer_close_icons = true,
+        show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
+        show_close_icon = true,
+        show_tab_indicators = true,
+        show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
+    }
+}
+
 require('vgit').setup({
   keymaps = {
     ['n <C-k>'] = function() require('vgit').hunk_up() end,
