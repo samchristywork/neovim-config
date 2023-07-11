af248651c12436b52e7add7275c007126d038061
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-10 20:50:05 -0500

    feat: Add several more movement options
    The "<" and ">" keys now scroll up and down five lines at a time, and
    "g-1" through "g9" navigate the file by percentages.

--- lua/core/movement.lua
+++ lua/core/movement.lua
@@ -10,3 +10,18 @@ vim.keymap.set('n', 'E', ':bp<cr>')
 
 -- Move lines
 vim.keymap.set('v', 's', ':sort<cr>')
+
+-- Scroll
+vim.keymap.set('n', '<', '5<c-e>')
+vim.keymap.set('n', '>', '5<c-y>')
+
+-- Navigate file by percentage
+vim.keymap.set('n', 'g1', ':normal! 10%<cr>zz')
+vim.keymap.set('n', 'g2', ':normal! 20%<cr>zz')
+vim.keymap.set('n', 'g3', ':normal! 30%<cr>zz')
+vim.keymap.set('n', 'g4', ':normal! 40%<cr>zz')
+vim.keymap.set('n', 'g5', ':normal! 50%<cr>zz')
+vim.keymap.set('n', 'g6', ':normal! 60%<cr>zz')
+vim.keymap.set('n', 'g7', ':normal! 70%<cr>zz')
+vim.keymap.set('n', 'g8', ':normal! 80%<cr>zz')
+vim.keymap.set('n', 'g9', ':normal! 90%<cr>zz')
