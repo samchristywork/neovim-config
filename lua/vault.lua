78a77e7890b34aeb5cb6e96ad53ed71cabcafafa
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-07-29 15:16:53 -0500

    feat: Add utility function to check if a file exists
    This function checks if a file exists by trying to open it as readable.
    It will be used for some LSP work I'm doing.

--- lua/vault.lua
+++ lua/vault.lua
@@ -210,3 +210,13 @@ vim.api.nvim_create_autocmd(
       end
     end,
   })
+
+function file_exists(name)
+  local f = io.open(name, "r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
