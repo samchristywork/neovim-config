1335b2bdd3ae61d5cb96fe229546d54b3b806c8a
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-11-17 19:22:36 -0600

    feat: Add a function to populate the quickfix list
    This function currently gets called every time Neovim is loaded which is
    pretty obnoxious, but I like it in principle.

--- lua/custom_commands.lua
+++ lua/custom_commands.lua
@@ -47,3 +47,29 @@ local function commands_picker()
     end,
   }):find()
 end
+
+-- Function to populate the quickfix list with lines not starting with whitespace
+local function populate_quickfix_non_whitespace()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+
+  local qf_items = {}
+
+  for i, line in ipairs(lines) do
+    if not line:match("^%s") and line ~= "" and line ~= "}" then
+      table.insert(qf_items, {
+        bufnr = bufnr,
+        lnum = i,
+        text = line
+      })
+    end
+  end
+
+  vim.fn.setqflist(qf_items)
+end
+
+vim.api.nvim_create_autocmd("UIEnter", {
+  callback = function()
+    populate_quickfix_non_whitespace()
+  end,
+})
