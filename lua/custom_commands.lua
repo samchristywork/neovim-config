57447521896f991519a7ee14207966b5ecb18de1
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-11-17 18:59:04 -0600

    feat: Add commands_picker function
    This function lists a bunch of functions in a Telescope window for the
    user to choose from.

---
+++ lua/custom_commands.lua
@@ -0,0 +1,40 @@
+local telescope = require('telescope')
+local pickers = require('telescope.pickers')
+local finders = require('telescope.finders')
+local conf = require('telescope.config').values
+
+local function commands_picker()
+  local commands = {
+  }
+
+  pickers.new({}, {
+    prompt_title = "Custom Commands",
+    finder = finders.new_table({
+      results = commands,
+      entry_maker = function(entry)
+        return {
+          value = entry.value,
+          display = entry.display,
+          ordinal = entry.ordinal,
+          execute_command = function()
+            vim.cmd(entry.value)
+          end,
+        }
+      end,
+    }),
+    sorter = conf.generic_sorter({}),
+    attach_mappings = function(prompt_bufnr, map)
+      map('i', '<CR>', function()
+        local selection = require('telescope.actions.state').get_selected_entry()
+        require('telescope.actions').close(prompt_bufnr)
+        selection.execute_command()
+      end)
+      map('n', '<CR>', function()
+        local selection = require('telescope.actions.state').get_selected_entry()
+        require('telescope.actions').close(prompt_bufnr)
+        selection.execute_command()
+      end)
+      return true
+    end,
+  }):find()
+end
