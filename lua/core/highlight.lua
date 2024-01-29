56686335f01477d16cee62fc7226f77d2ca769d7
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-01-28 21:53:03 -0600

    chore: Remove unnecessary configuration
    I am no longer using these highlight groups, so I have removed them.

--- lua/core/highlight.lua
+++ lua/core/highlight.lua
@@ -2,20 +2,9 @@
 vim.cmd("hi diffAdded ctermfg=green")
 vim.cmd("hi diffRemoved ctermfg=red")
 
--- Search
-vim.cmd("hi IncSearch cterm=reverse gui=reverse")
-vim.cmd("hi Search cterm=underline ctermfg=0 ctermbg=11 gui=underline guifg=#f0a0c0 guibg=#302028")
-
 -- Background
 vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
 vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE")
-
--- Status bar
-vim.cmd("hi StatusAccent guifg=#222222 guibg=#888888 ctermfg=0 ctermbg=46")
-vim.cmd("hi StatusMain guifg=#888888 guibg=NONE ctermbg=46 ctermfg=0")
-
--- Selection
-vim.cmd("hi Visual cterm=underline ctermfg=186 ctermbg=64 gui=underline guifg=#5f8700 guibg=#071000")
 
 -- Key command to interpret ASCII checkboxes
 vim.keymap.set("n", "<leader>h", function()
