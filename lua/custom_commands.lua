2d3582a6b4d82f6da8160002aa026240e5d37b71
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-01-29 18:25:55 -0600

    refactor: Remove duplicate table entries
    I was previously copying the same table values for the value, ordinal,
    and display parts of the table. Now we just set those programmatically.

--- lua/custom_commands.lua
+++ lua/custom_commands.lua
@@ -1,22 +1,24 @@
-local telescope = require('telescope')
 local pickers = require('telescope.pickers')
 local finders = require('telescope.finders')
 local conf = require('telescope.config').values
 
+local command_values = {
+	{ value = "GitGutter" },
+	{ value = "GitGutterNextHunk" },
+	{ value = "TSTextobjectGotoNextStart @function.outer" },
+	{ value = "cclose" },
+	{ value = "lua Search_all_files()" },
+	{ value = "lua Search_git_files()" },
+}
+
 local function commands_picker()
-	local commands = {
-		{ value = "GitGutter",              ordinal = "GitGutter",              display = "GitGutter" },
-		{ value = "cclose",                 ordinal = "cclose",                 display = "cclose" },
-		{ value = "norm yyp",               ordinal = "norm yyp",               display = "norm yyp" },
-		{ value = "GitGutterNextHunk",      ordinal = "GitGutterNextHunk",      display = "GitGutterNextHunk" },
-		{ value = "lua Search_git_files()", ordinal = "lua Search_git_files()", display = "lua Search_git_files()" },
-		{ value = "lua Search_all_files()", ordinal = "lua Search_all_files()", display = "lua Search_all_files()" },
-		{
-			value = "TSTextobjectGotoNextStart @function.outer",
-			ordinal = "TSTextobjectGotoNextStart @function.outer",
-			display = "TSTextobjectGotoNextStart @function.outer"
-		},
-	}
+	local commands = {}
+	for _, v in ipairs(command_values) do
+		local cmd = v.value
+		local display = cmd
+		local ordinal = cmd
+		table.insert(commands, { value = cmd, display = display, ordinal = ordinal })
+	end
 
 	pickers.new({}, {
 		prompt_title = "Custom Commands",
